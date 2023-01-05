8ed143681fbfb408f89639da436443a44deaa4a6
Constantine Fry <constantine.fry@gmail.com>
2023-01-05 20:23:35 +0000

    Import UIKIt.

--- EZCustomNavigation/EZNavigationControllerTransitionHelper+HandlePanGesture.swift
+++ EZCustomNavigation/EZNavigationControllerTransitionHelper+HandlePanGesture.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+import UIKit
 
 extension EZNavigationControllerTransitionHelper {
     @objc func handlePopSwipe(_ gestureRecognizer: UIScreenEdgePanGestureRecognizer) {
--- EZCustomNavigation/UINavigationController+SwizzledPushAndPop.swift
+++ EZCustomNavigation/UINavigationController+SwizzledPushAndPop.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 
 private let swizzling: (AnyClass, Selector, Selector) -> () = { forClass, originalSelector, swizzledSelector in
